5b35fd695429ff64ff14d3bdc3f0620259c78024
brozozowska <s_rozov@me.com>
2024-06-18 21:07:02 +0300

    Chapter 2. Customize views with properties - complete.

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,11 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+        }
+    }
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return .blue
+        } else {
+            return .yellow
+        }
+    }
+    
+    var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(day)
+                .font(.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(.largeTitle)
+                .padding(5)
+            Text("High: \(high)º")
+                .fontWeight(.semibold)
+            Text("Low: \(low)º")
+                .fontWeight(.medium)
+                .foregroundStyle(Color.secondary)
         }
         .padding()
     }
