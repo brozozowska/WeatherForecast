e9cbb3746136e64089657fcf5db1cb7b6697ca6f
brozozowska <s_rozov@me.com>
2024-06-18 20:11:24 +0300

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Сергей Розов on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
